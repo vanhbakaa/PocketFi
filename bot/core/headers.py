c30a3db51154c0039a3e74e3bceca668e6eddea3
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-06 22:17:48 +0700

    Update headers.py

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -11,7 +11,6 @@ headers = {
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'cross-site',
-    'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
     'Sec-Ch-Ua-mobile': '?1',
     'Sec-Ch-Ua-platform': '"Android"',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
